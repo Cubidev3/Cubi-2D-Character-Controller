3b210cc8332b72e8ca016b5c9da357fb980edfce
Cubidev3 <100206395+Cubidev3@users.noreply.github.com>
2023-10-03 12:41:44 -0300

    Add files via upload

--- Exemple/Player.gd
+++ Exemple/Player.gd
@@ -12,8 +12,12 @@ var diving: bool = false
 @export var in_apex_movement: HorizontalMovementInfo
 @export var jump_info: JumpInfo
 
-@export var dive_move_speed: float = 600
+@export var dive_move: HorizontalMovementInfo
 @export var dive_info: JumpInfo
+
+var side_flipping: bool = false
+@export var side_flip_movement: HorizontalMovementInfo
+@export var side_flip_info: JumpInfo 
 
 func _physics_process(delta):
 	var direction: int = Input.get_axis("left", "right")
@@ -23,20 +27,26 @@ func _physics_process(delta):
 	if not on_ground and is_on_floor():
 		on_ground = true
 		diving = false
+		side_flipping = false
 		jump_component.ground()
 		
 	elif on_ground and not is_on_floor():
 		on_ground = false
 		
-	if not diving:
-		if on_ground:
-			horizontal_movement.move(ground_movement, direction, delta)
+	if diving:
+		horizontal_movement.move(dive_move, direction, delta)
+		
+	elif side_flipping:
+		horizontal_movement.move(side_flip_movement, direction, delta)
+		
+	elif on_ground:
+		horizontal_movement.move(ground_movement, direction, delta)
 			
-		elif jump_component.in_apex():
-			horizontal_movement.move(in_apex_movement, direction, delta)
+	elif jump_component.in_apex():
+		horizontal_movement.move(in_apex_movement, direction, delta)
 			
-		else:
-			horizontal_movement.move(aerial_movement, direction, delta)
+	else:
+		horizontal_movement.move(aerial_movement, direction, delta)
 		
 	if diving:
 		jump_component.apply_gravity(dive_info, delta)
@@ -46,9 +56,12 @@ func _physics_process(delta):
 	
 	if jump:
 		if on_ground:
-			jump_component.jump(jump_info)
+			if horizontal_movement.turning_around(direction):
+				side_flip(direction)
+			else:
+				jump_component.jump(jump_info)
 			
-		elif not diving:
+		elif not diving and direction != 0:
 			dive(direction)
 		
 	if low_jump and not on_ground:
@@ -56,5 +69,11 @@ func _physics_process(delta):
 
 func dive(direction: int):
 	diving = true
+	side_flipping = false
 	jump_component.jump(dive_info)
-	velocity_component.set_velocity_x(dive_move_speed * direction)
+	velocity_component.set_velocity_x(dive_move.get_max_speed() * direction)
+
+func side_flip(direction: int):
+	side_flipping = true
+	jump_component.jump(side_flip_info)
+	velocity_component.set_velocity_x(side_flip_movement.get_initial_speed() * direction)
--- Scripts/HorizontalMovementComponent.gd
+++ Scripts/HorizontalMovementComponent.gd
@@ -12,10 +12,13 @@ func move(info: HorizontalMovementInfo, direction: int, delta: float):
 	elif velocity.is_inert_x():
 		velocity.apply_impulse_x(direction * info.get_initial_speed())
 		
-	elif velocity.going_right() and direction < 0 or velocity.going_left() and direction > 0:
+	elif turning_around(direction):
 		velocity.accelerate_x(direction * info.get_turn_acceleration(), delta)
 	
 	else:
 		velocity.accelerate_x(direction * info.get_acceleration(), delta)
 		
 	velocity.limit_speed_x(info.get_max_speed())
+
+func turning_around(direction: int) -> bool:
+	return velocity.going_right() and direction < 0 or velocity.going_left() and direction > 0
